21be9de1d46193ac388a2501978fb568fa00739f
Mike <mikej.miller440@gmail.com>
2021-12-11 09:37:12 -0600

    created elements for question buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
 // DOM selectors
+var quizGame = document.querySelector(".quiz")
 var quizButton = document.querySelector("#start-quiz");
 var highscoresRef = document.querySelector("#highscores-ref");
 var timeDisplay = document.querySelector("#time-display");
+var quizTitle = document.querySelector(".quiz-title");
+var quizP = document.querySelector(".quiz-descrip");
 
 // Quiz questions
 var firstQuestion = {
+    title: "This is question 1",
     name: "This is the question",
     optionA: "a",
     optionB: "b",
@@ -13,7 +17,33 @@ var firstQuestion = {
 }
 
 function startQuiz() {
-    console.log("The quiz has successfully started!");
+   console.log("the quiz has started!");
+   quizQustions();
+}
+
+function quizQustions() {
+    quizTitle.textContent = firstQuestion.title;
+    quizP.textContent = firstQuestion.name;
+    quizGame.removeChild(quizButton);
+
+    // create answer elements
+    // answer a
+    var firstOption = document.createElement("button");
+    firstOption.textContent = firstQuestion.optionA;
+    quizGame.appendChild(firstOption)
+    // answer b
+    var secondOption = document.createElement("button");
+    secondOption.textContent = firstQuestion.optionB;
+    quizGame.appendChild(secondOption);
+    // answer c
+    var thirdOption = document.createElement("button");
+    thirdOption.textContent = firstQuestion.optionC;
+    quizGame.appendChild(thirdOption);
+    // correct answer
+    var correctOption = document.createElement("button");
+    correctOption.textContent = firstQuestion.optionCorrect;
+    quizGame.appendChild(correctOption);
+    return;
 }
 
 // eventlistener for start quiz button
