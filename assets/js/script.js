3ea3af044f7dc637042190682d68fbd49e8203ab
Mike <mikej.miller440@gmail.com>
2021-12-11 09:11:32 -0600

    added potential setup for questions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,15 @@ var quizButton = document.querySelector("#start-quiz");
 var highscoresRef = document.querySelector("#highscores-ref");
 var timeDisplay = document.querySelector("#time-display");
 
+// Quiz questions
+var firstQuestion = {
+    name: "This is the question",
+    optionA: "a",
+    optionB: "b",
+    optionC: "c",
+    optionCorrect: "correct",
+}
+
 function startQuiz() {
     console.log("The quiz has successfully started!");
 }
